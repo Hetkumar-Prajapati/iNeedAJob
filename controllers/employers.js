a8e2890826924313de95abd0302712978f508d04
hetkumar1704 <200476049@student.gerogianc.on.ca>
2022-11-04 17:07:01 -0400

    authorization and env

--- controllers/employers.js
+++ controllers/employers.js
@@ -1,31 +1,104 @@
 const express = require('express')
 const router = express.Router()
 const Employer = require('../models/employer')
+const Region = require('../models/region')
+const passport = require('passport')
+
+// auth check to be called before any CUD method
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/auth/login')
+}
+
 
 // GET: /employers => show all employers
 router.get('/', (req, res) => {
     Employer.find((err, employers) => {
         if (err) {
             console.log(err);
-        } else {
-            res.render('employers/index'), {
+        } 
+        else {
+            res.render('employers/index', {
                 title: 'Employers', 
-                employers: employers
-            }
+                employers: employers,
+                user: req.user
+            })
         }
     })
 })
 
 // GET: /employers/create => display blank form
-router.get('/create', (req, res) => {
-    res.render('employers/create', { 
-        title: 'Add Employer'
-    })
+router.get('/create', isAuthenticated, (req, res) => {
+    // get regions for Form dropdown
+    Region.find((err, regions) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.render('employers/create', { 
+                title: 'Add Employer',
+                regions: regions,
+                user: req.user
+            })
+        }
+    }).sort('name')   
 })
 
 // POST: /employers/create => save new employer doc from form body
-router.post('/create', (req, res) => {
+router.post('/create', isAuthenticated, (req, res) => {
     Employer.create(req.body, (err, newEmployer) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.redirect('/employers')
+        }
+    })
+})
+
+// GET: /employers/delete/abc123 => remove selected Employer document
+router.get('/delete/:_id', isAuthenticated, (req, res) => {
+    Employer.remove({ _id: req.params._id }, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.redirect('/employers')
+        }
+    })
+})
+
+// GET: /employers/edit/abc123 => display populated form for editing
+router.get('/edit/:_id', isAuthenticated, (req, res) => {
+    // get regions for Form dropdown
+    Region.find((err, regions) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            // fetch selected Employer for display
+            Employer.findById(req.params._id, (err, employer) => {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    res.render('employers/edit', { 
+                        title: 'Employer Details',
+                        regions: regions,
+                        employer: employer,
+                        user: req.user
+                    })
+                }
+            })           
+        }
+    }).sort('name')   
+})
+
+// POST: /employers/edit/abc123 => update the db for the selected doc
+router.post('/edit/:_id', isAuthenticated, (req, res) => {
+    Employer.findByIdAndUpdate({ _id: req.params._id }, req.body, null, (err, employer) => {
         if (err) {
             console.log(err)
         }
